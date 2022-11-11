decdd56fcd4aaa45d902f472a9dbfdf6d800bee0
ArielLahiany <ariel.lahiany@gmail.com>
2022-11-11 15:40:28 +0200

    Adding basic modeling.

--- pkg/model/agent.go
+++ pkg/model/agent.go
@@ -1,1 +1,11 @@
 package model
+
+import (
+	"time"
+)
+
+type Agent struct {
+	ID        string    `db:"id"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
--- pkg/model/command.go
+++ pkg/model/command.go
@@ -1,1 +1,11 @@
 package model
+
+import (
+	"time"
+)
+
+type Command struct {
+	ID        string    `db:"id"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
