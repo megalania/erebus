6154663a10b883e49bb1a4f284128f6c6cf1ac93
ArielLahiany <ariel.lahiany@gmail.com>
2022-11-11 15:40:13 +0200

    Adding a basic gRPC server implementation.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,1 +1,37 @@
 package server
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+)
+
+func Run(addr string) error {
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	lis, err := net.Listen(
+		"tcp",
+		addr,
+	)
+	if err != nil {
+		return nil
+	}
+	var opts []grpc.ServerOption
+	server := grpc.NewServer(opts...)
+	cha := make(chan error)
+	go func() {
+		cha <- server.Serve(lis)
+	}()
+	select {
+	case <-shutdown:
+		log.Println(
+			"SIGINT has been received",
+		)
+		server.GracefulStop()
+	}
+	return nil
+}
