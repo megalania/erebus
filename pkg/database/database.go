35e84a73c2b780e7a9f00a46c20d1282d3182c18
ArielLahiany <ariel.lahiany@gmail.com>
2022-11-11 15:24:41 +0200

    Adding basic database pooling logic.

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Database struct {
+	Config pgxpool.Config
+	Pool   *pgxpool.Pool
+}
+
+func NewPool(str string) (*pgxpool.Pool, error) {
+	ctx := context.Background()
+	pool, err := pgxpool.New(
+		ctx,
+		str,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return pool, err
+}
