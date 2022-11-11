6bfb8e8a72557401ee0123b530c1cdb91350e376
ArielLahiany <ariel.lahiany@gmail.com>
2022-11-11 15:40:47 +0200

    Adding a basic serving logic.

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+
+	"github.com/megalania/erebus/pkg/server"
 )
 
+var (
+	host      string
+	port      int
+	version   string
+	timestamp string
+)
+
+func init() {
+	flag.StringVar(
+		&host,
+		"host",
+		"0.0.0.0",
+		"Specify the serving host",
+	)
+	flag.IntVar(
+		&port,
+		"port",
+		8080,
+		"Specify the serving port",
+	)
+	flag.Parse()
+}
+
 func main() {
-	log.Println("Hello, world!")
+	log.Printf(
+		"Erebus version %v (%v)",
+		version,
+		timestamp,
+	)
+	addr := fmt.Sprintf(
+		"%v:%v",
+		host,
+		port,
+	)
+	log.Printf(
+		"Starting server at %v",
+		addr,
+	)
+	log.Println(
+		"Stop the server with CONTROL-C",
+	)
+	err := server.Run(
+		addr,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
