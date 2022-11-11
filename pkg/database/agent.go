25f54444e9a0279880a9a79a2dc634f6fb0ca753
ArielLahiany <ariel.lahiany@gmail.com>
2022-11-11 15:35:36 +0200

    Adding basic database logic.

--- pkg/database/agent.go
+++ pkg/database/agent.go
@@ -1,1 +1,51 @@
 package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/megalania/erebus/pkg/model"
+)
+
+type AgentService struct {
+	Pool *pgxpool.Pool
+}
+
+func (a *AgentService) SelectSingle(ctx context.Context, id string) (*model.Agent, error) {
+	sql := "SELECT * FROM public.agent WHERE id=$1;"
+	row := a.Pool.QueryRow(
+		ctx,
+		sql,
+		id,
+	)
+	var agent model.Agent
+	err := row.Scan(
+		&agent.ID,
+		&agent.CreatedAt,
+		&agent.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &agent, nil
+}
+
+func (a *AgentService) DeleteSingle(ctx context.Context, id string) (*model.Agent, error) {
+	sql := "DELETE FROM public.agent WHERE id=$1;"
+	row := a.Pool.QueryRow(
+		ctx,
+		sql,
+		id,
+	)
+	var agent model.Agent
+	err := row.Scan(
+		&agent.ID,
+		&agent.CreatedAt,
+		&agent.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &agent, nil
+}
