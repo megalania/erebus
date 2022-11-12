9f3b13ebbfd59c5e5d36255d7a9c3a8133996d6a
ArielLahiany <ariel.lahiany@gmail.com>
2022-11-12 16:57:32 +0200

    Fixing the tag.

--- pkg/model/agent.go
+++ pkg/model/agent.go
@@ -7,5 +7,5 @@ import (
 type Agent struct {
 	ID        string    `db:"id" json:"ID" protobuf:"bytes,1,opt,name=id"`
 	CreatedAt time.Time `db:"created_at" json:"createdAt" protobuf:"bytes,2,opt,name=createdAt"`
-	UpdatedAt time.Time `db:"updated_at" json:"UpdatedAt" protobuf:"bytes,3,opt,name=updatedAt"`
+	UpdatedAt time.Time `db:"updated_at" json:"updatedAt" protobuf:"bytes,3,opt,name=updatedAt"`
 }
