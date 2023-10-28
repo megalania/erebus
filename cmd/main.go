da48c6c6a64b5e292995dda249c168a55d27be5a
Ariel Lahiany <ariel.lahiany@gmail.com>
2023-10-28 13:23:15 +0300

    Adding basic functionality.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,32 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"time"
+)
+
+const shutdownTimeout = 10 * time.Second
+
+var (
+	buildTimestamp string
+	buildVersion   string
+)
+
+func init() {
+	tiz := os.Getenv(
+		"TZ",
+	)
+	loc, err := time.LoadLocation(
+		tiz,
+	)
+	if err != nil {
+		log.Fatalln(
+			err.Error(),
+		)
+	}
+	time.Local = loc
+}
 
 func main() {
 	log.Println(
--- internal/node/node.go
+++ internal/node/node.go
@@ -1,1 +1,31 @@
 package node
+
+import (
+	"net"
+
+	"github.com/google/uuid"
+
+	"github.com/megalania/erebus/internal/network"
+)
+
+type Node struct {
+	ID uuid.UUID
+	PrivateIPv4Address net.IP
+	PublicIPv4Address  net.IP
+}
+
+func NewNode() (*Node, error) {
+	privateIPv4Address, err := network.GetPrivateIPv4Address()
+	if err != nil {
+		return nil, err
+	}
+	publicIPv4Address, err := network.GetPublicIPv4Address()
+	if err != nil {
+		return nil, err
+	}
+	return &Node{
+		ID: uuid.New(),
+		PrivateIPv4Address: privateIPv4Address,
+		PublicIPv4Address: publicIPv4Address,
+	}, nil
+}
