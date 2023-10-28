d1d6f3efe12af4dbbf1ac71b5e50731fc998441a
Ariel Lahiany <ariel.lahiany@gmail.com>
2023-10-28 14:05:02 +0300

    Adding and testing the GeneratePrivateRSAKey and GetPublicRSAKey methods.

--- internal/cryptography/cryptography.go
+++ internal/cryptography/cryptography.go
@@ -1,1 +1,25 @@
 package cryptography
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+func GeneratePrivateRSAKey(bitSize int) (*rsa.PrivateKey, error) {
+	privateRSAKey, err := rsa.GenerateKey(
+		rand.Reader,
+		bitSize,
+	)
+	if err != nil {
+		return nil, err
+	}
+	err = privateRSAKey.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return privateRSAKey, nil
+}
+
+func GetPublicRSAKey(privateRSAKey *rsa.PrivateKey) *rsa.PublicKey {
+	return privateRSAKey.Public().(*rsa.PublicKey)
+}
--- internal/cryptography/cryptography_test.go
+++ internal/cryptography/cryptography_test.go
@@ -1,1 +1,72 @@
 package cryptography_test
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+
+	"github.com/megalania/erebus/internal/cryptography"
+)
+
+const bitSize = 4096
+
+func TestGeneratePrivateRSAKey(t *testing.T) {
+	privateRSAKey, err := cryptography.GeneratePrivateRSAKey(
+		bitSize,
+	)
+	if err != nil {
+		t.Fatal(
+			err.Error(),
+		)
+	}
+	privateASNKey, err := x509.MarshalPKCS8PrivateKey(
+		privateRSAKey,
+	)
+	if err != nil {
+		t.Fatal(
+			err.Error(),
+		)
+	}
+	privatePEMKey := pem.EncodeToMemory(
+		&pem.Block{
+			Type: "RSA PRIVATE KEY",
+			Bytes: privateASNKey,
+		},
+	)
+	t.Logf(
+		"privatePEMKey: %s",
+		privatePEMKey,
+	)
+}
+
+func TestGetPublicRSAKey(t *testing.T) {
+	privateRSAKey, err := cryptography.GeneratePrivateRSAKey(
+		bitSize,
+	)
+	if err != nil {
+		t.Fatal(
+			err.Error(),
+		)
+	}
+	publicRSAKey := cryptography.GetPublicRSAKey(
+		privateRSAKey,
+	)
+	publicASNKey, err := x509.MarshalPKIXPublicKey(
+		publicRSAKey,
+	)
+	if err != nil {
+		t.Fatal(
+			err.Error(),
+		)
+	}
+	publicPEMKey := pem.EncodeToMemory(
+		&pem.Block{
+			Type: "RSA PUBLIC KEY",
+			Bytes: publicASNKey,
+		},
+	)
+	t.Logf(
+		"publicPEMKey: %s",
+		publicPEMKey,
+	)
+}
